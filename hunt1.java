86278a1b6dd91cdd397611a1edb1d21dab6babe1
priyanka9605 <priyasekar9605@gmail.com>
2016-08-22 18:59:49 +0530

    Update hunt1.java

--- hunt1.java
+++ hunt1.java
@@ -4,6 +4,28 @@ class hunt1
 {
   public static void main(String[] args)
   {
+     Scanner s = new Scanner(System.in);
+      int n = s.nextInt();
+       int count=1;
+      int a[] = new int[n];
+      for(int i=0;i<n;i++)
+       a[i] = s.nextInt();
+      Arrays.sort(a);
+      for(int i=0;i<n-1;i++)
+        {   count=1;
+           for(int j=i+1;j<n;j++)
+             {
+               if(a[i]==a[i+1])
+                  {
+                       a[i+1]='\0';
+                       count++;
+                  }
+             }
+          if(count>1)
+           System.out.println(a[i]);
+         }
+
+
     
   }
 }
